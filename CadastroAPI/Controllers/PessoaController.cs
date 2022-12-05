8e13d535c783d051ca10f56ee4f0bbf67471ce4e
Karlessandra Lima <karlessandralima@gmail.com>
2022-12-05 14:28:19 -0300

    Add project files.

---
+++ CadastroAPI/Controllers/PessoaController.cs
@@ -0,0 +1,100 @@
+﻿using CadastroAPI.Models;
+using CadastroAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PessoaController : Controller
+    { 
+        private readonly PessoaRepository pessoaRepository;
+
+        public PessoaController()
+        {
+            pessoaRepository = new PessoaRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Consultar()
+        {
+            try
+            {
+                var listaPessoa = pessoaRepository.Listar();
+                return Ok(listaPessoa);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        // Anotação de uso do Verb HTTP Get
+        [HttpGet]
+        [Route("Consultar/{id}")]
+        public ActionResult Consultar(int id)
+        {
+            try
+            {
+                var pessoa = pessoaRepository.Consultar(id);
+                if (pessoa.IdPessoa != 0)
+                {
+                    return Ok(pessoa);
+                }
+                else
+                {
+                    return NotFound();
+                }
+
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        // Anotação de uso do Verb HTTP Post
+        [HttpPost]
+        public ActionResult Cadastrar(Models.Pessoa pessoa)
+        {
+            try
+            {
+                pessoaRepository.Inserir(pessoa);
+                return Ok(pessoa);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPut]
+        public ActionResult Editar(Models.Pessoa pessoa)
+        {
+            try
+            {
+                pessoaRepository.Alterar(pessoa);
+                return Ok(pessoa);
+            }
+            catch (KeyNotFoundException)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult Excluir(int id)
+        {
+
+            try
+            {
+                pessoaRepository.Excluir(id);
+                return Ok();
+            }
+            catch (KeyNotFoundException)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ CadastroAPI/Models/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace CadastroAPI.Models
+{
+    public class Pessoa
+    {
+        public int IdPessoa { get; set; }
+        public String NomePessoa { get; set; }
+        public String EnderecoPessoa { get; set; }
+    }
+}
